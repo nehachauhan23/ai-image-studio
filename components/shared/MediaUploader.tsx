36f6e2d635a3efa685f9c31b2e9cabb58e73bbcb
Neha Chauhan <cneha15@gmail.com>
2025-01-04 12:08:07 +0530

    ...

--- components/shared/MediaUploader.tsx
+++ components/shared/MediaUploader.tsx
@@ -53,7 +53,7 @@ const MediaUploader = ({
 
   return (
     <CldUploadWidget
-      uploadPreset="jsm_imaginify"
+      uploadPreset="rkqowg6w"
       options={{
         multiple: false,
         resourceType: "image",
